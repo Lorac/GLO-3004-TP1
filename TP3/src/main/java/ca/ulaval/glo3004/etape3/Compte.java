d247fa766095337d4734f52a07eff75997506910
Maxime Charron <charron.maxime97@gmail.com>
2016-03-23 20:32:48 -0400

    Added compte

--- TP3/src/main/java/ca/ulaval/glo3004/etape3/Compte.java
+++ TP3/src/main/java/ca/ulaval/glo3004/etape3/Compte.java
@@ -1,4 +1,83 @@
 package ca.ulaval.glo3004.etape3;
 
 public class Compte {
+
+    private int solde;
+    private int nip;
+    private Date ouverture;
+    private Date fermeture;
+    private int liquide;
+    private final int maxLiquide = 1000;
+    private final int minSolde = 50;
+
+
+    public Compte(int solde, int nip, Date ouverture, Date fermeture){
+        if (solde < minSolde){
+            throw new IllegalArgumentException();
+        }
+        this.solde = solde;
+        this.nip = nip;
+        this.ouverture = ouverture;
+        this.fermeture = fermeture;
+        this.liquide = 0;
+    }
+
+    public int withdrawal(int amount){
+        if ( amount <= 0){
+            throw new IllegalArgumentException();
+        }
+        this.solde -= amount;
+        return solde;
+    }
+
+    public void deposit(int amount){
+        if ( amount <= 0){
+            throw new IllegalArgumentException();
+        }
+        this.solde += amount;
+    }
+
+    public void liquidDeposit(int amount){
+        if (amount <= 0 || this.liquide + amount > this.maxLiquide){
+            throw new IllegalArgumentException();
+        }
+        this.liquide += amount;
+        this.solde += amount;
+    }
+
+    public void resetLiquid(){
+        this.liquide = 0;
+    }
+
+    public void modifyPin(int newPin){
+        if (this.nip == newPin){
+            throw new IllegalArgumentException();
+        }
+        this.nip = newPin;
+    }
+
+    public int getSolde() {
+        return solde;
+    }
+
+    public int getNip() {
+        return nip;
+    }
+
+    public Date getOuverture() {
+        return ouverture;
+    }
+
+
+    public Date getFermeture() {
+        return fermeture;
+    }
+
+    public int getLiquide() {
+        return liquide;
+    }
+
+    public void setLiquide(int liquide) {
+        this.liquide = liquide;
+    }
 }
--- TP3/src/test/java/ca/ulaval/glo3004/etape3/CompteTest.java
+++ TP3/src/test/java/ca/ulaval/glo3004/etape3/CompteTest.java
@@ -1,7 +1,192 @@
 package ca.ulaval.glo3004.etape3;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CompteTest {
+
+    @Test
+    public void givenValidCompte_whenCreatingCompte_thenValidCompte() {
+        //given
+        int solde = 100;
+        int nip = 1234;
+        Date ouverture = null;
+        try {
+            ouverture = new Date(2, 2, 2016);
+        } catch (Exception e) {
+
+        }
+        //when
+        Compte compte = new Compte(solde, nip, ouverture, null);
+
+        //then
+        assertEquals(solde, compte.getSolde());
+        assertEquals(0, compte.getLiquide());
+        assertEquals(nip, compte.getNip());
+        assertEquals(ouverture, compte.getOuverture());
+        assertEquals(null, compte.getFermeture());
+    }
+
+    @Test
+    public void givenValidCompteWithFermeture_whenCreatingCompte_thenValidCompte() {
+        //given
+        int solde = 100;
+        int nip = 1234;
+        Date ouverture = null;
+        Date fermeture = null;
+        try {
+            ouverture = new Date(2, 2, 2016);
+            fermeture = new Date(3, 3, 2016);
+        } catch (Exception e) {
+
+        }
+        //when
+        Compte compte = new Compte(solde, nip, ouverture, fermeture);
+
+        //then
+        assertEquals(solde, compte.getSolde());
+        assertEquals(0, compte.getLiquide());
+        assertEquals(nip, compte.getNip());
+        assertEquals(ouverture, compte.getOuverture());
+        assertEquals(fermeture, compte.getFermeture());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void givenLowSolde_whenCreatingCompte_thenShouldFail() {
+        //given
+        int solde = 10;
+        int nip = 1234;
+        Date ouverture = null;
+        Date fermeture = null;
+        try {
+            ouverture = new Date(2, 2, 2016);
+            fermeture = new Date(3, 3, 2016);
+        } catch (Exception e) {
+
+        }
+        //when
+        Compte compte = new Compte(solde, nip, ouverture, fermeture);
+    }
+
+    @Test
+    public void givenValidCompte_whenWithdrawing_thenSoldeIsUpdated() {
+        //given
+        Compte compte = createValidCompte();
+        int soldeBefore = compte.getSolde();
+
+        //when
+        compte.withdrawal(100);
+
+        assertEquals(soldeBefore - 100, compte.getSolde());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void givenValidCompte_whenWithdrawingWithNegative_thenThrowsError() {
+        //given
+        Compte compte = createValidCompte();
+
+        //when
+        compte.withdrawal(-100);
+    }
+
+    @Test
+    public void givenValidCompte_whenDeposit_thenSoldeIsUpdated() {
+        //given
+        Compte compte = createValidCompte();
+        int soldeBefore = compte.getSolde();
+
+        //when
+        compte.deposit(100);
+
+        assertEquals(soldeBefore + 100, compte.getSolde());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void givenValidCompte_whenDepositWithNegative_thenThrowsError() {
+        //given
+        Compte compte = createValidCompte();
+
+        //when
+        compte.deposit(-100);
+    }
+
+    @Test
+    public void givenValidCompte_whenLiquidDeposit_thenSoldeAndLiquideIsUpdated() {
+        //given
+        Compte compte = createValidCompte();
+        int soldeBefore = compte.getSolde();
+        int liquidBefore = compte.getLiquide();
+
+        //when
+        compte.liquidDeposit(100);
+        //then
+        assertEquals(soldeBefore + 100, compte.getSolde());
+        assertEquals(liquidBefore + 100, compte.getLiquide());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void givenValidCompte_whenLiquidDepositWithNegative_thenThrowsError() {
+        //given
+        Compte compte = createValidCompte();
+
+        //when
+        compte.liquidDeposit(-100);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void givenValidCompte_whenLiquidDepositWithMaximumLiquid_thenThrowsError() {
+        //given
+        Compte compte = createValidCompte();
+
+        //when
+        compte.liquidDeposit(10000);
+    }
+
+    @Test
+    public void givenValidCompte_whenReset_thenLiquidIsReset() {
+        //given
+        Compte compte = createValidCompte();
+        compte.setLiquide(100);
+
+        //when
+        compte.resetLiquid();
+        //then
+        assertEquals(0, compte.getLiquide());
+    }
+
+    @Test
+    public void givenValidCompte_whenModifyPin_thenPinIsChanged() {
+        //given
+        Compte compte = createValidCompte();
+
+        //when
+        compte.modifyPin(2222);
+        //then
+        assertEquals(2222, compte.getNip());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void givenValidCompte_whenModifyPinWithSamePin_thenThrowException() {
+        //given
+        Compte compte = createValidCompte();
+
+        //when
+        compte.modifyPin(1234);
+    }
+
+
+    private Compte createValidCompte() {
+        int solde = 1000;
+        int nip = 1234;
+        Date ouverture = null;
+        Date fermeture = null;
+        try {
+            ouverture = new Date(2, 2, 2016);
+        } catch (Exception e) {
+
+        }
+        return new Compte(solde, nip, ouverture, fermeture);
+    }
 
 }
